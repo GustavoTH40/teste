9ebf15b55849f80cb827392189c8ca64f0795654
GustavoTH40 <96442299+GustavoTH40@users.noreply.github.com>
2024-01-31 14:16:05 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,8 +1,31 @@
 if game.PlaceId == 15991080927 then
---load
+
+--load SORCE
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+--MENU
+local Window = OrionLib:MakeWindow({Name = "JRC MENU", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+
+--VALOR
+_G.ESP =true
+
+--funcao
+function ESP()
+while ESP == true then
+    local gui = Instance.new ("SurfaceGui", parte)
+    gui.Name = "ESP"
+    gui.AlwaysOnTop = true
+    gui.LightInfluence = 0
+    gui.Size = UDim2.new (1.75, 0, 1.75, 0)
+    local frame = Instance.new ("Frame", gui)
+    frame.BackgroundColor3 = Color3.fromRGB (170, 0, 0)
+    frame.Size = UDim2.new (1, 0, 1, 0)
+    frame.BorderSizePixel = 4
+    frame.BorderColor3 = Color3.fromRGB (0, 0, 0)
+    end
+end
+
 
 
 --jogador
@@ -10,6 +33,19 @@ local JogadorTab = Window:MakeTab({
 	Name = "Jogador",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
+})
+
+--SECAO
+local Section = JogadorTab:AddSection({
+	Name = "VISUAL"
+})
+JogadorTab:AddToggle({
+	Name = "ESP",
+	Default = false,
+	Callback = function(Value)
+		_G.ESP =true
+        ESP()
+	end    
 })
 
 end
