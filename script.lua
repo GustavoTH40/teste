5ee9267b28545bff997e3505380a1b06ef842fcd
GustavoTH40 <96442299+GustavoTH40@users.noreply.github.com>
2024-03-26 17:11:00 -0300

    Delete script.lua

--- script.lua
+++
@@ -1,5 +0,0 @@
--- Script para dar kick em todos os jogadores do roblox
-local players = game:GetService ("Players") -- obtém o serviço de jogadores
-for i,v in ipairs (players:GetPlayers ()) do -- percorre todos os jogadores
-  v:kick ("Motivo") -- v é a instância/objeto do jogador
-end
